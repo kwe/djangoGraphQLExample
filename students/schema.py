a5072b8f8e8c008339c4f58525ced0e4f64db630
Kevin Evans <kwevans@gmail.com>
2018-03-12 16:54:34 +0000

    adding courses

--- students/schema.py
+++ students/schema.py
@@ -4,23 +4,31 @@ from graphene_django.types import DjangoObjectType
 
 from .models import Student, Course
 
+
 class StudentType(DjangoObjectType):
-  class Meta:
-    model = Student
+    class Meta:
+        model = Student
+
 
 class CourseType(DjangoObjectType):
-  class Meta:
-    model = Course
+    class Meta:
+        model = Course
+
 
 class Query(object):
-  student = graphene.Field(StudentType,
-                            id = graphene.Int()
-                            )
-  all_students = graphene.List(StudentType)
+    student = graphene.Field(StudentType,
+                             id=graphene.Int()
+                             )
+    all_students = graphene.List(StudentType)
 
-  def resolve_student(self, info, **kwargs):
-    id = kwargs.get('id')
-    return Student.objects.get(pk=id)
-    
-  def resolve_all_students(self, info, **kwargs):
-    return Student.objects.all()
+    all_courses = graphene.List(CourseType)
+
+    def resolve_student(self, info, **kwargs):
+        id = kwargs.get('id')
+        return Student.objects.get(pk=id)
+
+    def resolve_all_students(self, info, **kwargs):
+        return Student.objects.all()
+
+    def resolve_all_courses(self, info, **kwargs):
+        return Course.objects.all()
